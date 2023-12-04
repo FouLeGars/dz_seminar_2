08d82fa39a14c7f925090bbdb14061d6d4d3ad9c
Karen Martirosyan <m.k.a@list.ru>
2023-12-04 09:25:06 +0300

    Отформатировал внешний вид задачи 1

--- Zadacha_1/Program.cs
+++ Zadacha_1/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 class Program
 {
-static void Main()
-{
-Console.Write("Введите число a: ");
-int a = Convert.ToInt32(Console.ReadLine());
-if (a % 7 == 0 && a % 23 == 0)
-{
-Console.WriteLine("да");
-}
-else
-{
-Console.WriteLine("нет");
-}
-}
+    static void Main()
+    {
+        Console.Write("Введите число a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a % 7 == 0 && a % 23 == 0)
+        {
+            Console.WriteLine("да");
+        }
+        else
+        {
+            Console.WriteLine("нет");
+        }
+    }
 }
